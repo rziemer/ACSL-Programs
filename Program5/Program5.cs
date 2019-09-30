49b88f24bd098efe1d050e084c1b740d9ca0b8be
Rhys Ziemer <rhysz@microsoft.com>
2019-09-30 19:14:32 -0400

    Initial Build of Program5

---
+++ Program5/Program5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Program5
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            List<string> fileList = new List<string>();   
+            string fileLine = "";
+            StreamReader sr = new StreamReader(args[0]);
+            while ((fileLine = sr.ReadLine()) != null)
+                fileList.Add(fileLine);
+            string[] fileString = fileList.ToArray();
+
+            for (int fileloop = 0; fileloop < fileString.Length; fileloop++)
+            {      
+                char[] delimiters = {','};
+                string[] parsedString = fileString[fileloop].Split(delimiters);
+
+                acslURL sourceURL = new acslURL(parsedString[0].Trim());
+                acslURL destinationURL = new acslURL(parsedString[1].Trim());
+                Console.WriteLine(sourceURL.mergeURL(destinationURL).ToString());
+            }
+        }
+    }
+}
