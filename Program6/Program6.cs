10d0d1d6f9d8761e908894f18e30a5d8edc42cba
Rhys Ziemer <rhysz@microsoft.com>
2019-09-30 21:18:18 -0400

    Initial Build of Program6

---
+++ Program6/Program6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Program6
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            List<string> fileList = new List<string>();   
+            string fileLine = "";
+            StreamReader sr = new StreamReader(args[0]);
+            while ((fileLine = sr.ReadLine()) != null)
+                fileList.Add(fileLine);
+            string[] fileString = fileList.ToArray();
+
+            for (int fileloop = 0; fileloop < fileString.Length; fileloop++)
+            {      
+                char[] delimiters = {','};
+                string[] parsedString = fileString[fileloop].Split(delimiters);
+            }
+        }
+    }
+}
